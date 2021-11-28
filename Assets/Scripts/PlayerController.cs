98d687389cb88965d2152079846da6bda8a777ba
gudmunduro <goh00@protonmail.com>
2021-11-28 21:29:13 +0000

    Add basic character to test character animation

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator characterAnimator;
+    public Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterAnimator.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        {
+            if (!characterAnimator.enabled)
+            {
+                characterAnimator.enabled = true;
+            }
+        }
+        else
+        {
+            if (characterAnimator.enabled)
+            {
+                characterAnimator.enabled = false;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(0.01f, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-0.01f, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(new Vector2(0, 260.0f));
+        }
+    }
+}
