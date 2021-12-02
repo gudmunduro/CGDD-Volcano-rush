16ae4a2e5bc3243e69ef5784818b17607eb68135
gudmunduro <goh00@protonmail.com>
2021-12-02 20:15:27 +0000

    Add x axis camera lock

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -21,21 +21,46 @@ public class CameraController : MonoBehaviour
     private void Update()
     {
         var position = player.position;
+        
+        var camHeight = 2f * _camera.orthographicSize;
+        var camWidth = camHeight * _camera.aspect;
+
+        _fixCameraPos(ref position, camWidth, camHeight);
 
         _cameraPos = new Vector3(position.x, position.y, -10f);
         transform.position = Vector3.SmoothDamp(gameObject.transform.position, _cameraPos, ref _velocity, dampTime);
     }
 
-    private bool _isInTilemapBounds(Vector2 position)
+    private bool _isInTilemapBounds(Vector2 position, Vector2 size)
     {
         var tilemapX = tilemap.transform.position.x;
         var tilemapY = tilemap.transform.position.y;
-        var halfTilemapX = (tilemap.size.x / 2);
-        var halfTilemapY = (tilemap.size.y);
+        var halfTilemapX = tilemap.size.x / 2;
+        var halfTilemapY = tilemap.size.y / 2;
+        // y + height > tilemapY - 1 && y - height < tilemapY + halfTilemapY - 1;
 
-        Debug.Log($"X of tilemap: {tilemapX + halfTilemapX}, X of cam: {position.x}");
+        return (position.x - size.x > tilemapX - 1 && position.x + size.x < tilemapX + halfTilemapX - 1)
+               && (position.y + size.y > tilemapY - 1 && position.y - size.y < tilemapY + halfTilemapY - 1);
+    }
 
-        return (position.x > tilemapX - 2 && position.x < tilemapX + halfTilemapX);
-        //(position.y > tilemapY && position.y < tilemapY + halfTilemapY);
+    private void _fixCameraPos(ref Vector3 position, float width, float height)
+    {
+        var tilemapX = tilemap.transform.position.x;
+        var tilemapY = tilemap.transform.position.y;
+        var halfTilemapX = tilemap.size.x / 2;
+        var halfTilemapY = tilemap.size.y / 2;
+
+        if (!(position.x - width / 2 > tilemapX - 0.5f))
+        {
+            position.x = tilemapX + width / 2 - 0.5f;
+        }
+        else if (!(position.x + width / 2 < tilemapX + halfTilemapX - 1.5f))
+        {
+            position.x = tilemapX + halfTilemapX - width / 2 - 1.5f;
+        }
+        else if (position.y + height > (tilemapY + halfTilemapY / 4) + 0.1f)
+        {
+            position.y = tilemapY + halfTilemapY / 4 - height + 0.07f;
+        }
     }
 }
