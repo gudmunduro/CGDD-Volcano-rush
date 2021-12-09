81defbe7bc581af50947f5ba7f9b0940734ebebf
Jónas <jonasfb20@ru.is>
2021-12-09 20:07:05 +0000

    Added visual feedback for the cool zones

--- Assets/Scripts/CoolZones.cs
+++ Assets/Scripts/CoolZones.cs
@@ -1,15 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class CoolZones : MonoBehaviour
 {
     
     private PlayerController2 _playercontroller;
+    private Light2D _light;
+    private float _fadeSpeed = 1f;
+    
     // Start is called before the first frame update
     void Start()
     {
         _playercontroller = GameManager.instance.player.GetComponent<PlayerController2>();
+        _light = transform.GetChild(0).GetComponent<Light2D>();
     }
     
     private void OnTriggerEnter2D(Collider2D other)
@@ -21,9 +27,10 @@ public class CoolZones : MonoBehaviour
             if (transform.position.y < _playercontroller.playerYposition)
             {
                 _playercontroller.ChangePosition(transform.position.x, transform.position.y);
-                
-                
-                
+
+
+                _light.intensity = 3;
+
             }
         }
     }
@@ -33,6 +40,18 @@ public class CoolZones : MonoBehaviour
         if (other.gameObject.name == "CoolZoneSensor")
         {
             FindObjectOfType<Overheating>().incoolzone = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (_light.intensity > 1)
+        {
+            _light.intensity -= _fadeSpeed * Time.deltaTime;
+        }
+        else
+        {
+            _light.intensity = 1;
         }
     }
 }
