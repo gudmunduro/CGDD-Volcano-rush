a4516ddcbcf849bc2d031c35558432e38039964b
gudmunduro <goh00@protonmail.com>
2021-12-10 16:27:31 +0000

    Add particles when arrow crashes into wall

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -1,15 +1,24 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
+    public GameObject explodeParticleEmitter;
+
     private SoundManager _soundManager;
     private Rigidbody2D _rigidbody;
+    private Transform _arrowEnd;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _soundManager = SoundManager.instance;
+    }
+
+    private void Start()
+    {
+        _arrowEnd = transform.Find("ArrowEnd");
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -30,13 +39,22 @@ public class Arrow : MonoBehaviour
                 playerAnimateObject.DamagePlayerHealth(20.0f);
             }
         }
-        else 
+        else
         {
-            float playerDistance = Vector2.Distance(GameObject.Find("Player").transform.position, transform.position);
+            var playerDistance = Vector2.Distance(GameManager.instance.player.transform.position, transform.position);
             if (playerDistance < 10)
                 SoundManager.instance.PlaySound(SoundType.ArrowImpact);
+
+            _createDestroyParticles();
         }
+
         Destroy(gameObject);
+    }
+
+    private void _createDestroyParticles()
+    {
+        var arrowEndPos = _arrowEnd.position;
+        Instantiate(explodeParticleEmitter, new Vector3(arrowEndPos.x, arrowEndPos.y, -0.1f), Quaternion.identity);
     }
 
     private Direction? _currentDirection()
@@ -47,6 +65,7 @@ public class Arrow : MonoBehaviour
         {
             return Direction.Right;
         }
+
         if (velocity.x < 0)
         {
             return Direction.Left;
